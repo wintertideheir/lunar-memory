5d245c9842bc83434aa985149180b7b6f2e73acb
Sagun Singh <wintertideheir@users.noreply.github.com>
2020-11-18 17:19:35 -0500

    Implement a forgetting curve function
    Implement a forgetting curve function, based off of the research of
    Hermann Ebbinghaus. This function will be essential in predicting how
    note scores decay over time.

--- src/notes.c
+++ src/notes.c
@@ -1,4 +1,5 @@
 #include <time.h>
+#include <math.h>
 
 /* The maximum score possible for a note. Because the maxiumum reaction
  * time of a human is around 0.2 seconds, the maximum score is it's
@@ -37,3 +38,31 @@ struct Note
     int tags_len;
     char **tags;
 };
+
+/* The forgetting curve (called the Ebbinghaus curve in Lunar Memory
+ * for easier differentiation) describes how a particular kind of
+ * memory failiure, transience, relates to time.
+ *
+ * This particular function uses a single free parameter rather than
+ * the original two parameters, has a maximum of 1f rather than 100f,
+ * and is shifted to the left by 1f.
+ *
+ * Parameters:
+ *     * time, a unitless meausure of how long time has passed since
+ *       the last review. In practice it has units of days. Must be
+ *       between 0f and FLT_MAX (see float.h).
+ *     * alpha, a unitless free parameter. Must be non-zero and
+ *       positive.
+ *
+ * Returns:
+ *     A number between 1f and 0f that predicts what percent of
+ *     information is retained after a given length of time. The return
+ *     value can be reinterpreted as the chance for any randomly
+ *     selected fragment of information being remembered after the
+ *     given length of time.
+ */
+float ebbinghaus_curve(float time,
+                       float alpha)
+{
+    return 1f / ((alpha * logf(time + 1f)) + 1f);
+}
